48961ca10bfc4afe3fc836f0ff03340d72905285
macro_reports <macro_reports@github.com>
2018-02-21 22:02:04 -0500

    latest commit

--- monetary_policy.py
+++ monetary_policy.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 20 19:39:43 2018
+Created on Tue Feb 20 16:17:41 2018
 
-@author: Pedro
+@author: pportocarrero
 """
+# CODE TO SET THE DEFAULT WORKING DIRECTORY
 
 import pandas as pd
 
@@ -14,7 +14,11 @@ import pickle
 
 os.chdir('/Users/Pedro/Desktop/monetary_policy')
 
+#D:\pportocarrero@mef.gob.pe\Investor Relations Office\Projects\monetary_policy
+#/Users/Pedro/Desktop/monetary_policy
+
 cwd = os.getcwd()
+
 
 #### LOADS EXCEL FILES AND PUTS THEM IN PANDAS 
 
@@ -30,7 +34,8 @@ expectations_db = pd.ExcelFile(expectations_fl)
 
 interest_rate_db = pd.ExcelFile(interest_rate_fl)
 
-### LOADS DATAFRAMES AND SAVES THEM AS PICKLES
+
+#### LOADS DATAFRAMES AND SAVES THEM AS PICKLES
 
 inflation_df = inflation_db.parse('Hoja1')
 
@@ -44,7 +49,7 @@ interest_rate_df = interest_rate_db.parse('Hoja1')
 
 interest_rate_df.to_pickle('interest_rate_df.pickle')
 
-## LOAD PICKLES DATASETS AND DELETES DEPRECATED FILES
+#### LOAD PICKLES DATASETS AND DELETES DEPRECATED FILES
 
 inflation_data = pd.read_pickle('inflation_df.pickle')
 
@@ -63,3 +68,25 @@ del expectations_fl
 del interest_rate_df
 
 del interest_rate_fl
+
+#### NEW DATA ON LAST ROW OF DATAFRAMES
+
+expectations_data.loc[len(expectations_data)] = ['Jan', 'January', '01', '18', 2018, 58.5, 70.7, 50, 2.23, 'nan', 2.4, 2.5]
+
+expectations_data.to_pickle('expectations_df.pickle')
+
+inflation_data.loc[len(inflation_data)] = ['Jan', 'January', '01', '18', 2018, 0.127417107488597, 1.2531883694175, 0.127417107488597, -0.129632120477368, 1.97121356769657]
+
+inflation_data.to_pickle('inflation_df.pickle')
+
+interest_rate_data.loc[len(interest_rate_data)] = ['Jan', 'January', '01', '18', 2018, 3.00, 2, 3, 1]
+
+interest_rate_data.to_pickle('interest_rate_df.pickle')
+
+#### LOAD DATA
+
+inflation_data = pd.read_pickle('inflation_df.pickle')
+
+expectations_data = pd.read_pickle('expectations_df.pickle')
+
+interest_rate_data = pd.read_pickle('interest_rate_df.pickle')
